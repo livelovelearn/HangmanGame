0729321b0ee1d0d64f238c98eda567485e498334
S <s>
2013-08-03 19:44:51 -0400

    {"type":"TI", "runs":"1"}

--- Hangman.java
+++ Hangman.java
@@ -17,9 +17,31 @@ public class Hangman extends ConsoleProgram {
     	int random = rgen.nextInt (0, 10);
     	String word = lex.getWord(random);
     	int length = word.length();
+    	int chance = 8;
     	
+    	println("Welcom to Hangman!");
     	print("The word now looks like this: ");
     	for (int i=0;i<length;i++) print("-");
+    	
+    	String str1= readLine("Your guess: ");
+    	char ch1=str1.charAt(0);
+    	if (ch1-'A'>=0&&ch1-'A'<=25)
+    		{ch1=(char)(ch1-'A'+'a');}
+    	
+    	while(ch1-'a'<0||ch1-'a'>25)
+    		{str1=("illegal character, please guess again: ");
+    		 ch1=str1.charAt(0);
+    		 if (ch1-'A'>=0&&ch1-'A'<=25)
+     		     {ch1=(char)(ch1-'A'+'a');}
+    		}
+    	
+    	int pos1= word.indexOf(ch1);
+    	if (pos1==-1)
+    		{println("There are no " + ch1 + "'s in the word.");
+    	     chance--;
+    	     }
+    	
+    	
 	}
 
     private HangmanLexicon lex = new HangmanLexicon();
