5358c13f0cb017223e925631573ab0152ccb5b8c
S <s>
2013-08-03 19:10:45 -0400

    {"type":"TI", "runs":"1"}

--- Hangman.java
+++ Hangman.java
@@ -14,7 +14,14 @@ import java.awt.*;
 public class Hangman extends ConsoleProgram {
 
     public void run() {
-		/* You fill this in */
+    	int random = rgen.nextInt (0, 10);
+    	String word = lex.getWord(random);
+    	int length = word.length();
+    	
+    	print("The word now looks like this: ");
+    	for (int i=0;i<length;i++) print("-");
 	}
 
+    private HangmanLexicon lex = new HangmanLexicon();
+    private RandomGenerator rgen = RandomGenerator.getInstance();
 }
