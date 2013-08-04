2f9a071286d6d06a27deba77bb53f363a3f7c9e0
S <s>
2013-08-04 08:53:48 -0400

    {"type":"TI", "runs":"1"}

--- Hangman.java
+++ Hangman.java
@@ -28,7 +28,7 @@ public class Hangman extends ConsoleProgram {
     	print("The word now looks like this: ");
     	for (int i=0;i<length;i++) 
     		  covered += '-';
-    	println();
+    	println(covered);
     	
      while(chance>0){
     	String str1= readLine("Your guess: ");
