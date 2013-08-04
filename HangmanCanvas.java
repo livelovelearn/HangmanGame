b941f5cc52d47ff6b50717761f7837e2cccaa442
S <s>
2013-08-04 10:53:56 -0400

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -27,9 +27,9 @@ public class HangmanCanvas extends GCanvas {
  * been guessed so far; unguessed letters are indicated by hyphens.
  */
 	public void displayWord(String word) {
-		/*GRect rect = new GRect(0,getHeight()*0.9, getWidth(), getHeight()*0.1);
+		GRect rect = new GRect(0,getHeight()*0.9, getWidth(), getHeight()*0.1);
 		rect.setFilled(true);
-		rect.setFillColor(Color.RED);*/
+		rect.setFillColor(Color.RED);
 		GLabel wordlabel = new GLabel(word, getWidth()/2, getHeight()*0.9);
 		add (wordlabel);
 	}
