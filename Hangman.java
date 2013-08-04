3980bb542ffa7ddcae5c2efa5a17dd86d90337e1
S <s>
2013-08-04 10:27:08 -0400

    {"type":"TI", "runs":"1"}

--- Hangman.java
+++ Hangman.java
@@ -52,6 +52,7 @@ public class Hangman extends ConsoleProgram {
 
 			int pos1 = word.indexOf(ch1);
 			if (pos1 == -1) {
+				canvas.noteIncorrectGuess(ch1);
 				println("There are no " + ch1 + "'s in the word.");
 				chance--;
 				println("You have " + chance + " guesses left.");
